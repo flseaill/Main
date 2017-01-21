e71e55aaae531e35d1b49e4fb366b4355b83a197
Florian SEAILLES <flseaill@e1r6p18.42.fr>
2017-01-21 23:30:56 +0100

    libftv01

---
+++ libft/backup/libftv01/in_progress/started/a_tester/ft_strjoin.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: flseaill <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/01/21 23:00:11 by flseaill          #+#    #+#             */
+/*   Updated: 2017/01/21 23:06:41 by flseaill         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	int i;
+	int j;
+	char *str;
+
+	i = 0;
+	j = 0;
+	if (!str)
+		return (NULL);
+	str = (char *)(malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2))));
+	while (s1[j])
+	{
+		str[i] = s1[j];
+		i++;
+		j++;
+	}
+	j = 0;
+	while (s2[j])
+	{
+		str[i] = s2[j];
+		i++;
+		j++;
+	}
+	str[i] = '\0';
+	return (str);
+}
