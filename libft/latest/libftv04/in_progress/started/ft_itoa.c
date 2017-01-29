e51d4df1fd4d023acfbcc90bc7972e0ede110fb7
Florian Seailles <seailles42@gmail.com>
2017-01-29 09:15:44 +0100

    libftv04

--- libft/latest/libftv04/in_progress/started/ft_itoa.c
+++ libft/latest/libftv04/in_progress/started/ft_itoa.c
@@ -10,6 +10,39 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+int		ft_lennum(int n)
+{
+	int i;
+
+	if (n == 0)
+		return (1);
+	i = 0;
+	while (n != 0)
+	{
+		i++;
+		n = n / 10;
+	}
+	return (i);
+}
+
+int		ft_sign(int n)
+{
+	if (n < 0)
+		return (1);
+	return (0);
+}
+
+int		ft_nosign(int n)
+{
+	int nosign;
+
+	nosign = n;
+	if (n < 0)
+		nosign = -n;
+	return (nosign);
+
+}
+
 char	*ft_itoa(int n)
 {
 	
