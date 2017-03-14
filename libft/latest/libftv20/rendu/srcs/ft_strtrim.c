3b21208936512be8c470ada3f69e404bd5f73f21
Florian SEAILLES <flseaill@e1r3p19.42.fr>
2017-03-14 20:04:14 +0100

    putain de strtrim.c

--- libft/latest/libftv20/rendu/srcs/ft_strtrim.c
+++ libft/latest/libftv20/rendu/srcs/ft_strtrim.c
@@ -6,37 +6,64 @@
 /*   By: flseaill <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/05 17:48:48 by flseaill          #+#    #+#             */
-/*   Updated: 2017/03/05 19:16:50 by flseaill         ###   ########.fr       */
+/*   Updated: 2017/03/14 20:03:41 by flseaill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/libft.h"
+#include <stdio.h>
+
+int		ft_is_blank(char c)
+{
+	if (c == ' ' || c == '\t' || c == '\n')
+		return (1);
+	else
+		return (0);
+}
 
 char	*ft_strtrim(char const *s)
 {
-	size_t	i;
-	size_t	j;
-	char	*cpy;
-	char	*str;
+	size_t i;
+	size_t j;
+	size_t k;
+	size_t len;
+	char *cpy;
 
 	i = 0;
 	j = 0;
-	if (!s)
-		return (NULL);
-	str = (char *)malloc(sizeof(char) * ft_strlen(s) + 1);
-	str = ft_strcpy(str, s);
-	cpy = (char *)malloc(sizeof(char) * ft_strlen(s));
-	while (str[i] && cpy)
+	k = 0;
+	len = ft_strlen(s);
+	printf("%s", "Premiere boucle = ");
+	while (ft_is_blank(s[i]) == 1)
 	{
-		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n')
-			i++;
-		else
-		{
-			cpy[j] = str[i];
-			j++;
-			i++;
-		}
+		i++;
+		len--;
 	}
-	cpy[i] = '\0';
+	printf("%zu\n", i);
+	printf("%s", "Deuxieme boucle = ");
+	while (ft_is_blank(s[j]) == 1)
+	{
+		j--;
+		len--;
+	}
+	printf("%zu\n", j);
+	if (!(cpy = (char *)malloc(sizeof(char) * (ft_strlen(s) - len + 1))))
+		return (NULL);
+	while (s[i] != s[j])
+	{
+		cpy[k] = s[j];
+		printf("%c\n", cpy[k]);
+		j++;
+		k++;
+	}
+	cpy[k] = '\0';
+	//printf("%s", ft_strdup(cpy));
 	return (ft_strdup(cpy));
+}
+
+int		main(void)
+{
+	const char s[] = "	 \n\ntest	test\ntest\n\n\n";
+	ft_strtrim(s);
+	//printf("\n%s", ft_strtrim(s));
 }
